d2e6536ce45ba4103e4909af7af119f36064e28a
Pankaj Prajapati <114794372+pankaj143p@users.noreply.github.com>
2024-10-06 22:16:10 +0530

    cadcn added

--- video-generator/app/page.js
+++ video-generator/app/page.js
@@ -1,10 +1,12 @@
 import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
    <>
     <div className='text-red-300' >
       <h1>Video Generator</h1>
+      <Button variant="destructive">Hello Design</Button>
     </div>
    </>
   );
