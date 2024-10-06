881abcfb7bdc527da99f4cd69bf92d54425c9a41
Pankaj Prajapati <114794372+pankaj143p@users.noreply.github.com>
2024-10-06 22:03:34 +0530

    upload

---
+++ video-generator/app/page.js
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+   <>
+    <div className='text-red-300' >
+      <h1>Video Generator</h1>
+    </div>
+   </>
+  );
+}
