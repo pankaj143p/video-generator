7dcc323909b0aa6bf3841bb106883ba07ef9fde3
Pankaj Prajapati <114794372+pankaj143p@users.noreply.github.com>
2024-10-07 19:23:48 +0530

    design

--- video-generator/app/dashboard/page.jsx
+++ video-generator/app/dashboard/page.jsx
@@ -1,10 +1,14 @@
+'use client'
 import React from 'react'
-
-const Dashboard = ({children}) => {
+import {Button} from '@/components/ui/button'
+const Dashboard = () => {
   return (
     <div>
-    {children}
-      this is dashboard
+    
+     <div className='flex items-center justify-between p-4'>
+         <h1>Dashboard</h1>
+         <Button className='bg-purple-950'>+Create New</Button>
+     </div>
     </div>
   )
 }
