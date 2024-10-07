e6995d23aab098388359905338f38e76697f680b
Pankaj Prajapati <114794372+pankaj143p@users.noreply.github.com>
2024-10-07 15:04:02 +0530

    header work

--- video-generator/app/_components/header/page.jsx
+++ video-generator/app/_components/header/page.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
-
+import Image from 'next/image';
+import { Button } from '@/components/ui/button';
+import { UserButton } from '@clerk/nextjs';
+// import {logo} from ''
 const Header = () => {
   return (
-    <div>
-      Hey I'm header
+    <div className='p-3 px-5 flex items-center justify-between shadow-md'>
+      <div className='flex flex-row gap-2 items-center'>
+        <Image src={"/logo.svg"} width={40} height={40}></Image>
+        <h4 className='text-blue-800 font-bold text-2xl '>Video Generater</h4>
+      </div>
+      <div className='flex gap-3 items-center'>
+        <Button className='bg-teal-700'>Dashboard</Button>
+        <UserButton></UserButton>
+      </div>
     </div>
   );
 }
