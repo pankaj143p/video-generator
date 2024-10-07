ace50534fc5b5bda24e2b37380e02338e2d9b60b
Pankaj Prajapati <114794372+pankaj143p@users.noreply.github.com>
2024-10-07 13:40:43 +0530

    dashboard files added

---
+++ video-generator/app/_components/header/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div>
+      Hey I'm header
+    </div>
+  );
+}
+
+export default Header;
---
+++ video-generator/app/dashboard/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Dashboard = ({children}) => {
+  return (
+    <div>
+    {children}
+      this is dashboard
+    </div>
+  )
+}
+
+export default Dashboard
