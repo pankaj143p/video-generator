f284785d252be63e146a9159e3c26f39d1e2673d
Pankaj Prajapati <114794372+pankaj143p@users.noreply.github.com>
2024-10-08 08:18:01 +0530

    add sec key

--- video-generator/middleware.js
+++ video-generator/middleware.js
@@ -1,17 +1,37 @@
+// import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+
+
+// const isProtectedRoute = createRouteMatcher(['/dashboard(.*)'])
+
+// export default clerkMiddleware((auth, req) => {
+//   if (isProtectedRoute(req)) auth().protect()
+// })
+
+// export const config = {
+//   matcher: [
+   
+//     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+   
+//     '/(api|trpc)(.*)',
+//   ],
+// };
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-
-const isProtectedRoute = createRouteMatcher(['/dashboard(.*)'])
+const isProtectedRoute = createRouteMatcher(['/dashboard(.*)']);
 
 export default clerkMiddleware((auth, req) => {
-  if (isProtectedRoute(req)) auth().protect()
-})
+  if (!process.env.CLERK_SECRET_KEY) {
+    console.error('Missing CLERK_SECRET_KEY');
+  }
+
+  if (isProtectedRoute(req)) {
+    auth().protect();
+  }
+});
 
 export const config = {
   matcher: [
-   
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-   
     '/(api|trpc)(.*)',
   ],
 };
