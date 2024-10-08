53ac47c7101f094b9aa1c8f1ebb39ffbf114bf5f
Pankaj Prajapati <114794372+pankaj143p@users.noreply.github.com>
2024-10-08 08:50:56 +0530

    redirect to sign-in

--- video-generator/next.config.mjs
+++ video-generator/next.config.mjs
@@ -1,8 +1,26 @@
-// next.config.js
+// // next.config.js
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//     env: {
+//       CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
+//     },
+//   };
+  
+//   export default nextConfig;
+  
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     env: {
       CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
+    },
+    async redirects() {
+      return [
+        {
+          source: '/',
+          destination: '/sign-in',  // Adjusted path to your sign-in page
+          permanent: false,
+        },
+      ];
     },
   };
   
