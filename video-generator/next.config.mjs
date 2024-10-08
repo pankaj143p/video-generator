909dae67a73507f2243ef636993977fccb4383fa
Pankaj Prajapati <114794372+pankaj143p@users.noreply.github.com>
2024-10-08 08:16:15 +0530

    add sec key

--- video-generator/next.config.mjs
+++ video-generator/next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
-
+// next.config.js
+module.exports = {
+    env: {
+      CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
+    },
+  };
+  
 export default nextConfig;
