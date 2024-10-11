b7db4e3cae1b2586b5d79f29771dac01867fa7b7
Pankaj Prajapati <114794372+pankaj143p@users.noreply.github.com>
2024-10-11 09:58:22 +0530

    selected topics concnate with create new page

--- video-generator/app/dashboard/create-new/_components/select-topic.jsx
+++ video-generator/app/dashboard/create-new/_components/select-topic.jsx
@@ -3,15 +3,13 @@ import React, { useState } from 'react'
 import {
     Select,
     SelectContent,
-    SelectGroup,
     SelectItem,
-    SelectLabel,
     SelectTrigger,
     SelectValue,
 } from "@/components/ui/select"
 import { Textarea } from '@/components/ui/textarea'
 
-const SelectTopic = () => {
+const SelectTopic = ({ onUserSelect }) => {
     const [selectedTopic, setSelectedTopic] = useState('')
 
     const topicOptions = [
@@ -27,7 +25,10 @@ const SelectTopic = () => {
             {/* Topic selection for generating video */}
             <h2 className='font-bold text-xl text-blue-800'>Video Content</h2>
             <p className='text-gray-500'>What is the topic of your videos</p>
-            <Select onValueChange={(value) => setSelectedTopic(value)}>
+            <Select onValueChange={(value) => {
+                setSelectedTopic(value)
+                value !== 'Custom Prompt' && onUserSelect('topic', value)
+            }}>
                 <SelectTrigger className="w-full mt-2 p-6 text-lg">
                     <SelectValue placeholder="Select topic for video" value={selectedTopic} />
                 </SelectTrigger>
@@ -40,8 +41,12 @@ const SelectTopic = () => {
                 </SelectContent>
             </Select>
 
-            {selectedTopic=='Custom Prompt' && 
-            <Textarea className='mt-3' placeholder='write Prompt which you want to create video' ></Textarea> }
+            {selectedTopic === 'Custom Prompt' && 
+            <Textarea className='mt-3'
+                onChange={(e) => onUserSelect('topic', e.target.value)}
+                placeholder='Write the prompt for the video you want to create'
+            />
+            }
         </div>
     )
 }
--- video-generator/app/dashboard/create-new/page.jsx
+++ video-generator/app/dashboard/create-new/page.jsx
@@ -1,14 +1,22 @@
-import React from 'react'
+'use client'
+import React, { useState } from 'react'
 import SelectTopic from './_components/select-topic'
 
 const CreateNew = () => {
+
+  const [formData, setFormData] = useState([]);
+  const onHandleInputChange=(fieldName, fieldValue)=>{
+     console.log(fieldName,fieldValue);
+     
+
+  }
   return (
     <div className='md:px-20' >
 
      <h2 className='font-bold text-4xl text-blue-800 text-center'>Create New</h2>
      <div className='mt-10 shadow-md p-10'>
       {/*  topics selection  */}
-      <SelectTopic></SelectTopic>
+      <SelectTopic onUserSelect={onHandleInputChange} ></SelectTopic>
       
       {/*  styles for selection */} 
 
