e40ab71dafd2df70bf282f99ec7910c79c1dad12
Pankaj Prajapati <114794372+pankaj143p@users.noreply.github.com>
2024-10-12 20:16:22 +0530

    bug fixes

--- video-generator/app/dashboard/create-new/_components/custom-loading.jsx
+++ video-generator/app/dashboard/create-new/_components/custom-loading.jsx
@@ -1,3 +1,28 @@
+// import React from 'react'
+// import Image from 'next/image'
+// import {
+//     AlertDialog,
+//     AlertDialogContent,
+    
+// } from "@/components/ui/alert-dialog"
+
+// function LoadingEffect({loading}) {
+//     return (
+//         <div>
+//             <AlertDialog open={loading} >
+//                 <AlertDialogContent>
+//                     <div className='flex flex-col items-center'>
+//                         <Image  src={'/loading.gif'} width={100} height={100}></Image>
+//                         <h1 className='text-md font-bold'>Generating your video... Please don't refresh the page</h1>
+//                     </div>
+//                 </AlertDialogContent>
+//             </AlertDialog>
+//         </div>
+//     )
+// }
+
+// export default LoadingEffect;
+
 import React from 'react'
 import Image from 'next/image'
 import {
@@ -12,8 +37,8 @@ function LoadingEffect({loading}) {
             <AlertDialog open={loading} >
                 <AlertDialogContent>
                     <div className='flex flex-col items-center'>
-                        <Image alt='loading' src={'/loading.gif'} width={100} height={100}></Image>
-                        <h1 className='text-md font-bold'>Generating your video... Please don't refresh the page</h1>
+                        <Image alt='image loading' src={'/loading.gif'} width={100} height={100}></Image>
+                        <h1 className='text-md font-bold'>Generating your video... Please don&#39;t refresh the page</h1>
                     </div>
                 </AlertDialogContent>
             </AlertDialog>
