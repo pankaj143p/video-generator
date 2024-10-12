da5e3a976c069bf27548fd0e5e583dc87b34e4fa
Pankaj Prajapati <114794372+pankaj143p@users.noreply.github.com>
2024-10-12 20:07:17 +0530

    debuged

--- video-generator/app/dashboard/create-new/_components/custom-loading.jsx
+++ video-generator/app/dashboard/create-new/_components/custom-loading.jsx
@@ -2,14 +2,8 @@ import React from 'react'
 import Image from 'next/image'
 import {
     AlertDialog,
-    AlertDialogAction,
-    AlertDialogCancel,
     AlertDialogContent,
-    AlertDialogDescription,
-    AlertDialogFooter,
-    AlertDialogHeader,
-    AlertDialogTitle,
-    AlertDialogTrigger,
+    
 } from "@/components/ui/alert-dialog"
 
 function LoadingEffect({loading}) {
@@ -18,7 +12,7 @@ function LoadingEffect({loading}) {
             <AlertDialog open={loading} >
                 <AlertDialogContent>
                     <div className='flex flex-col items-center'>
-                        <Image src={'/loading.gif'} width={100} height={100}></Image>
+                        <Image alt='loading' src={'/loading.gif'} width={100} height={100}></Image>
                         <h1 className='text-md font-bold'>Generating your video... Please don't refresh the page</h1>
                     </div>
                 </AlertDialogContent>
