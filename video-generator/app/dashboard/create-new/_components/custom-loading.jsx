2f4d9a12828dc0896dab62abde2d86056815269e
Pankaj Prajapati <114794372+pankaj143p@users.noreply.github.com>
2024-10-13 16:08:57 +0530

    voice testing

--- video-generator/app/dashboard/create-new/_components/custom-loading.jsx
+++ video-generator/app/dashboard/create-new/_components/custom-loading.jsx
@@ -1,28 +1,3 @@
-// import React from 'react'
-// import Image from 'next/image'
-// import {
-//     AlertDialog,
-//     AlertDialogContent,
-    
-// } from "@/components/ui/alert-dialog"
-
-// function LoadingEffect({loading}) {
-//     return (
-//         <div>
-//             <AlertDialog open={loading} >
-//                 <AlertDialogContent>
-//                     <div className='flex flex-col items-center'>
-//                         <Image  src={'/loading.gif'} width={100} height={100}></Image>
-//                         <h1 className='text-md font-bold'>Generating your video... Please don't refresh the page</h1>
-//                     </div>
-//                 </AlertDialogContent>
-//             </AlertDialog>
-//         </div>
-//     )
-// }
-
-// export default LoadingEffect;
-
 import React from 'react'
 import Image from 'next/image'
 import {
