03aadd5c004a427291937cfcc77842d5dc5324ec
Pankaj Prajapati <114794372+pankaj143p@users.noreply.github.com>
2024-11-07 10:01:24 +0530

    conflict resolve

--- video-generator/app/page.js
+++ video-generator/app/page.js
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import { Button } from "@/components/ui/button";
+import { Button } from "../components/ui/button";
 import { SignIn , UserButton} from "@clerk/nextjs";
 import Dashboard from "./dashboard/page";
 
